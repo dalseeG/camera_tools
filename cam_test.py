4832078f9d8d4057aea25b67e9ece4be0f3dc6cb
dalseeG <eugeneroh.s@gmail.com>
2020-10-21 11:47:41 -0700

    I2C added

--- cam_test.py
+++ cam_test.py
@@ -1,49 +1,67 @@
-#!/usr/bin/python3
-#
-# Simple test program used for testing
-# the new Galaxycore sensor's V4L driver
-# and grabbing images for the image
-# processing team to analyze
-#
-
 import numpy as np
 import cv2 as cv
+import time
+import smbus
 
+######## I2C Setting ########
+# I2C Bus
+i2c_ch = 0
 
-def on_gain(val):
-    print("gain %d" % val)
+# DAC Slave address
+i2c_slave = 0x60
+
+# Power Down bits
+PD_normal = 0x00
+
+# Bus Initialization
+bus = smbus.SMBus(i2c_ch)
+
+def write_dac(value):
+	bus.write_byte_data(i2c_slave, PD_normal, value)
+	return 0 
+
+######## GUI Setting ########
+def on_voltage(val):
+	print("voltage %d" % val)
+	write_dac(val)
 
 def on_exposure(val):
-    print("exposure %d" % val)
+	print("Exposure %d" % val)
 
-def on_voltage(val):
-    print("voltage %d" % val)
+def on_gain(val):
+	print("Gain %d" % val)
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
 
 cv.namedWindow('frame')
-cv.createTrackbar('gain', 'frame', 0, 255, on_gain)
-cv.createTrackbar('exposure', 'frame', 0, 255, on_exposure)
-cv.createTrackbar('film voltage', 'frame', 0, 255, on_voltage)
+
+cv.createTrackbar('Film Voltage', 'frame', 0, 255, on_voltage)
+cv.createTrackbar('Exposure', 'frame',0, 255, on_exposure)
+cv.createTrackbar('Gain', 'frame', 0, 255, on_gain)
+
 
 
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+	cv.imshow('frame', img)
+	pos = cv.getTrackbarPos('Film Voltage', 'frame')
+	font = cv.FONT_HERSHEY_SIMPLEX	
+	cv.putText(img, str(pos), (50, 150), font, 6, (255, 255, 255), 10)
+	if cv.waitKey(0) == ord('q'):
+		break
+
+
+
+"""
+print("Hello Python")
+time.sleep(0.5)
+print("Writing DAC values...")
+write_dac(0xff)
+time.sleep(2)
+print("Writing the next DAC values...")
+write_dac(0x00)
+time.sleep(2)
+print("DAC value setting completed")
+"""
